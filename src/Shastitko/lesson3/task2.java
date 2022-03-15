eaf2fa915cba58c0f8b0aa2a2056245a811ffbd8
89max89 <100864953+89max89@users.noreply.github.com>
2022-03-16 01:20:20 +0300

    DoTwo

---
+++ src/Shastitko/lesson3/task2.java
@@ -0,0 +1,15 @@
+package Shastitko.lesson3;
+
+public class task2 {
+        public static void main(String[] args) {
+    int a=100;
+    int b=5;
+    int c=6;
+
+    if (a+b>c & a+c>b & b+c>a) {
+        System.out.println("треугольник существует" );}
+        else
+        System.out.println("треугольник не существует" );
+    }
+
+}
