f4a9ff35e69cecf22d4572535fd5a01734bd06c7
89max89 <100864953+89max89@users.noreply.github.com>
2022-03-16 14:06:40 +0300

    ForAlexandra2

--- src/Shastitko/lesson3/Task1.java
+++ src/Shastitko/lesson3/Task1.java
@@ -3,25 +3,28 @@ package Shastitko.lesson3;
 public class Task1 {
     public static void main(String[] args) {
 
-        int rondom=-19;
-        int number=(rondom/10);
+        int rondom=29;
+        int number=rondom/10;
 
 
-        if (number >= 0) {
-            System.out.println(" число положительное" );        }
+        if (rondom>= 0) {
+            System.out.println(" число положительное" );}
         else System.out.println(" число отрицательное" );
 
-        if (number < 0){
+        if (number < 1){
             number=number*-1;
         }
 
-        if (number < 1) {
-            System.out.println(" однозначное" );}
-        else  number=number/10;
+        if (number < 1 ) {
+            System.out.println(" однозначное" );
+            number=number/10;}
+        else   if (  number < 1){
+            System.out.println(" двухзначное " );
+            number=number/10;}
+        else if (number < 1){
+            System.out.println(" трехзначное " );}
 
-        if (number < 1){
-            System.out.println(" двухзначное " );}
-        else System.out.println(" трехзначное " );
+
 
         }
 
@@ -30,4 +33,4 @@ public class Task1 {
 
     }
 
-}
+
